7ce6fc3cec9c02a79b8e3a844b3b2fe1cf31d7f0
Alex Coco <alex.coco@gmail.com>
2014-05-17 22:08:39 -0400

    Integrate app with json responses

--- static/application.js
+++ static/application.js
@@ -1,17 +1,60 @@
 $(function() {
   var pkgnamefield = $('#pkgname');
+  var validMessage = $('#validmessage');
+  var invalidMessage = $('#invalidmessage');
+
+  function hideMessages() {
+    validMessage.hide();
+    invalidMessage.hide();
+    invalidMessage.find('ul').empty();
+  }
+
+  function showValidPkgname(name) {
+    validMessage.find('.name').text(name);
+    validMessage.show();
+  }
+
+  function showInvalidPkgname(name, causes) {
+    var ul = invalidMessage.find('ul');
+
+    causes.forEach(function(cause) {
+      var li = document.createElement('li');
+      li.innerText = cause;
+      
+      ul.append(li);
+    });
+
+    invalidMessage.find('.name').text(name);
+    invalidMessage.show();
+  }
 
   function afterValidate(data) {
+    hideMessages();
+
+    if (data.error != '') {
+      console.log(data.error);
+      return;
+    }
+
     console.log(data);
+
+    if (data.success === false) {
+      showInvalidPkgname(data.pkgname, data.causes);
+    } else {
+      showValidPkgname(data.pkgname);
+    }
   }
 
   $('#pkgnameform').on('submit', function(e) {
     e.preventDefault();
 
+    var name = pkgnamefield.val().trim();
+    if (name == '') return;
+
     $.ajax({
       url: '/validate',
       type: 'post',
-      data: { pkgname: pkgnamefield.val() },
+      data: { pkgname: name },
       success: afterValidate
     });
   });
@@ -19,8 +62,6 @@ $(function() {
   $('#generate').on('click', function(e) {
     e.preventDefault();
 
-    $.get('/generate', function(data) {
-      console.log(data);
-    });
+    $.get('/generate', afterValidate);
   });
 })
